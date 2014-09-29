27a1a8756c24d448ad9b9f4a0bd05df2546516ec
theJWong <wong.jerry08@gmail.com>
2014-09-29 15:52:14 -0700

    remove twitch_fun.py
    remove twitch_fun.py

--- twitch_fun.py
+++
@@ -1,9 +0,0 @@
-import urllib2
-import json
-
-url = 'https://api.twitch.tv/kraken'
-contents = urllib2.urlopen(url)
-json_output = json.loads(contents.read())
-output = json.dumps(json_output, sort_keys=True, indent=4, separators=(',', ': '))
-
-print output
